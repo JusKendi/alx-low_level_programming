88df34175774f07e200b97a5ddbd866de9ba6b3f
JusKendi <kendibrenda20@gmail.com>
2023-05-27 07:49:57 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,32 +1,46 @@
-#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
 /**
-* read_textfile - reads a text file and prints it to the POSIX standard out
-* @filename: name of the file to read
-* @letters: number of characters to print
-* Return: 0 on success
-**/
+* read_textfile - prints text from a file
+*
+* @filename: name of the file
+* @letters: number of characters to read
+*
+* Return: actual number of letters read, 0 if end of file
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int txt_file, total, read_status;
-char buffer[BUFSIZE];
+int file;
+int length, wrotechars;
+char *buf;
 
-if (filename == NULL)
+if (filename == NULL || letters == 0)
 return (0);
-txt_file = open(filename, O_RDONLY);
-if (txt_file == -1)
+buf = malloc(sizeof(char) * (letters));
+if (buf == NULL)
 return (0);
-total = 0;
-read_status = 1;
-while (letters > BUFSIZE && read_status != 0)
+
+file = open(filename, O_RDONLY);
+if (file == -1)
 {
-read_status = read(txt_file, buffer, BUFSIZE);
-write(STDOUT_FILENO, buffer, read_status);
-total += read_status;
-letters -= BUFSIZE;
+free(buf);
+return (0);
 }
-read_status = read(txt_file, buffer, letters);
-write(STDOUT_FILENO, buffer, read_status);
-total += read_status;
-close(txt_file);
-return (total);
+length = read(file, buf, letters);
+if (length == -1)
+{
+free(buf);
+close(file);
+return (0);
+}
+
+wrotechars = write(STDOUT_FILENO, buf, length);
+
+free(buf);
+close(file);
+if (wrotechars != length)
+return (0);
+return (length);
 }
